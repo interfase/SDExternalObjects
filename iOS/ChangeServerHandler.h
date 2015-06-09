7be03027fa6df72ff3e15fac302958c4f4f69275
mauricio <mauricio.maden89@gmail.com>
2015-06-09 15:56:09 -0400

    c

--- iOS/ChangeServerHandler.h
+++
@@ -1,8 +0,0 @@
-#import <GXFlexibleClient/GXFlexibleClient.h>
-
-@interface ChangeServerHandler : GXActionExternalObjectHandler
-
-+ (BOOL)canHandleAction:(id <GXActionExternalObjectDescriptor>)actionExObjDesc;
-- (void)actionExecuteWithContext:(id)contextEntityData delegate:(id)delegate;
-
-@end
