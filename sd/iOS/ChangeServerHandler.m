b0b55b68168fdd966b2631e43fc75762a77a6c14
mauricio <mauricio.maden89@gmail.com>
2015-07-07 11:40:14 -0400

    cambio en la recarga de la aplicacion

--- sd/iOS/ChangeServerHandler.m
+++ sd/iOS/ChangeServerHandler.m
@@ -2,6 +2,7 @@
 
 @interface ChangeServerHandler (Private)
 - (void)handleChangeServerAction;
+- (void)handleChangeServerFirstAction;
 - (NSString*)getFirstParameter;
 
 @end
@@ -17,7 +18,7 @@
 + (BOOL)canHandleAction:(id )actionExObjDesc {
 if (actionExObjDesc && [@"ChangeServer" isEqualToString:[actionExObjDesc actionCallObjectName]]) {
   NSString *exObjMethod = [actionExObjDesc actionExternalObjectMethod];
-  if ([exObjMethod hasPrefix:@"Server"]
+  if ([exObjMethod hasPrefix:@"Server"] || [exObjMethod hasPrefix:@"ServerFirst"] 
             )
         {
    return YES;
@@ -33,25 +34,38 @@ return NO;
     NSString *exObjMethod = [self.actionExObjDesc actionExternalObjectMethod];
 	if ([exObjMethod isEqualToString:@"Server"]) {
 	  [self handleChangeServerAction];
-	} 
+	}
+    if ([exObjMethod isEqualToString:@"ServerFirst"]) {
+        [self handleChangeServerFirstAction];
+    }
 }
 
 - (void)handleChangeServerAction {
-    
+
     NSString *message = [self getFirstParameter];
-    
+
     [[AppDelegate_Shared currentInstance] saveServerAddressAndURLWithValue:message];
     UIWindow *win= [[AppDelegate_Shared currentInstance] window];
-    
+
     win.rootViewController = [[[self gxActionHandlerViewController] actionHandlerViewController]initWithNibName:nil bundle:nil];
-    
+
     [[AppDelegate_Shared currentInstance] loadModelAndViews:NO];
      /*NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
      [defaults setObject:message forKey:@"server_address_preference"];
      [defaults synchronize];*/
     
+    //NSTimeInterval interval= 5.0;
+    //[self onFinishedExecutingWithSuccessAfterDelay:interval];
+    [self onFinishedExecutingWithSuccess];
+}
+- (void)handleChangeServerFirstAction {
+    
+    NSString *message = [self getFirstParameter];
+    
+    [[AppDelegate_Shared currentInstance] saveServerAddressAndURLWithValue:message];
+    [[AppDelegate_Shared currentInstance] loadModelAndViews:NO];
 
-    //[self onFinishedExecutingWithSuccess];
+    [self onFinishedExecutingWithSuccess];
 }
 
 - (NSString*)getFirstParameter {
